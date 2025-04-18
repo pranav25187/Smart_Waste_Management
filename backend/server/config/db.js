07275006158e8ff9d6a409d7c4d0a4e206329743
pranav25187 <pranavborse2023@gmail.com>
2025-04-19 01:09:39 +0530

    Update db.js

--- backend/server/config/db.js
+++ backend/server/config/db.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+// Create a pool of connections
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 10,  // Maximum number of connections in the pool
+  queueLimit: 0  // Unlimited queue length
 });
 
-module.exports = pool.promise(); // Use promise wrapper
+// Export the pool wrapped with promises
+module.exports = pool.promise();
