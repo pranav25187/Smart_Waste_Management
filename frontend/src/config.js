086acac694396d7449b6a223263b5776bb86ef07
pranav25187 <pranavborse2023@gmail.com>
2025-04-19 09:26:29 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
 // frontend/src/config.js
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://smart-waste-management-eob6.vercel.app';
+export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'https://smart-waste-management-eob6.vercel.app';
